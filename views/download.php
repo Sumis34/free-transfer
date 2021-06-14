12dc070d230db8ffb42aaf644e7928260a9a9bd3
Noe <eon.sberk@gmail.com>
2021-06-14 20:10:16 +0200

    insert file data to db

--- views/download.php
+++ views/download.php
@@ -1,5 +1,22 @@
-<h1>Download</h1>
 <?php
-print_r($_SESSION['testUrl']);
+
 echo $_SESSION['fileToGet'];
+?>
+<div id="main-bg">
+    <div class="upload">
+        <!-- File upload form -->
+        <form id="uploadForm" enctype="multipart/form-data">
+            <label>Choose File:</label>
+            <input type="file" name="file" id="fileInput">
+            <input type="submit" name="submit" value="UPLOAD" />
+        </form>
+        <!-- Progress bar -->
+        <div class="progress">
+            <div class="progress-bar"></div>
+        </div>
+        <!-- Display upload status -->
+        <div id="uploadStatus"></div>
+    </div>
+</div>
+
 
