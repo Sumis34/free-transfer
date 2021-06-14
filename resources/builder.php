125d542ed7a96afbff50075999e30e6ee7cb0f8b
Noe <eon.sberk@gmail.com>
2021-06-14 13:39:43 +0200

    testing upload.js

--- resources/builder.php
+++ resources/builder.php
@@ -18,6 +18,7 @@ function build($page)
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
         <script src="https://kit.fontawesome.com/c38bf06c9c.js" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="/transfer/js/upload.js"></script>
         <link rel="shortcut icon" type="image/png" href="/images/favicon.png">
         <title>Free Transfer</title>
     </head>
--- views/home.php
+++ views/home.php
@@ -1,8 +1,16 @@
 <div id="main-bg">
     <div class="upload">
-        <form action="">
-            <input type="file">
-            <button type="submit">Send</button>
+        <!-- File upload form -->
+        <form id="uploadForm" enctype="multipart/form-data">
+            <label>Choose File:</label>
+            <input type="file" name="file" id="fileInput">
+            <input type="submit" name="submit" value="UPLOAD" />
         </form>
+        <!-- Progress bar -->
+        <div class="progress">
+            <div class="progress-bar"></div>
+        </div>
+        <!-- Display upload status -->
+        <div id="uploadStatus"></div>
     </div>
 </div>
