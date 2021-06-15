0c69391b00c7a7ccc97b52d689f4c085e03a79cc
Noe <eon.sberk@gmail.com>
2021-06-15 20:47:29 +0200

    temp home button

--- views/upload/sendFile.php
+++ views/upload/sendFile.php
@@ -5,12 +5,11 @@ $fileCode = $_SESSION['fileCode'];
 // like this: (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/file/$fileCode";
 
 //This link is the download link the user gets
-
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/transfer/file/$fileCode";
 ?>
 <div id="main-bg">
     <div class="share" id="share">
-
         <p style="color:#28A74B;" class="success">File has uploaded successfully!</p> <input type="text" name="link" value="<?php echo $actual_link ?>"></input>
+        <a href="home " class="btn btn-primary">home</a>
     </div>
 </div>
