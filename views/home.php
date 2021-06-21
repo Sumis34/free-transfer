4bb982d60d058772ef50118d6ae6f71b90d6535c
Noe <eon.sberk@gmail.com>
2021-06-21 21:39:16 +0200

    social media links

--- views/home.php
+++ views/home.php
@@ -10,6 +10,11 @@
             <input type="file" name="file" id="fileInput" hidden>
         </form>
         <p class="hint">Click, to upload files</p>
-        
+    </div>
+    <div class="social-links">
+        <div class="row">
+            <div class="col"><a href="youtube.com/channel/UCrMHiwrrOGY3sAtQV3KgZyA" target="_blank"><i class="fab fa-youtube"></i></a></div>
+            <div class="col"><a href="https://www.instagram.com/noekrebs/" target="_blank"><i class="fab fa-instagram"></i></a></div>
+        </div>
     </div>
 </div>
