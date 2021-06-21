6729780db309ab4dbda22d3beca3302b2106daa4
Noe <eon.sberk@gmail.com>
2021-06-21 22:12:10 +0200

    link fixed

--- views/socialmediaLinks.php
+++ views/socialmediaLinks.php
@@ -1,6 +1,6 @@
 <div class="social-links">
     <div class="row">
-        <div class="col"><a href="youtube.com/channel/UCrMHiwrrOGY3sAtQV3KgZyA" target="_blank"><i class="fab fa-youtube"></i></a></div>
+        <div class="col"><a href="https://youtube.com/channel/UCrMHiwrrOGY3sAtQV3KgZyA" target="_blank"><i class="fab fa-youtube"></i></a></div>
         <div class="col"><a href="https://www.instagram.com/noekrebs/" target="_blank"><i class="fab fa-instagram"></i></a></div>
     </div>
 </div>
